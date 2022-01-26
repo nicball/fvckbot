9fb93e8e6b2c0f26df274156edd6a374745cc2a3
Nick Ballard <znhihgiasy@gmail.com>
2022-01-26 12:45:16 +0800

    update

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ mkDerivation, aeson, async, base, bytestring, direct-sqlite, hint
+, http-conduit, lens, lens-aeson, lib, random, silently
+, sqlite-simple, text, time
+}:
+mkDerivation {
+  pname = "fvckbot";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    aeson async base bytestring direct-sqlite hint http-conduit lens
+    lens-aeson random silently sqlite-simple text time
+  ];
+  homepage = "https://github.com/nicball/fvckbot#readme";
+  license = lib.licenses.bsd3;
+}
