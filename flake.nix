61022c520ed573e2906dd505fed9af4cf1eb0542
Nick Ballard <znhihgiasy@gmail.com>
2024-01-20 01:21:31 +0800

    better code

--- flake.nix
+++ flake.nix
@@ -2,21 +2,23 @@
   description = "fvckbot";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils }:
+    let overlay = self: super: {
+      fvckbot = self.haskellPackages.callPackage ./default.nix {};
+      fvckbot-docker =
+        self.dockerTools.buildImage {
+          name = "fvckbot";
+          tag = "latest";
+          config.Entrypoint = "${self.fvckbot}/bin/fvckbot";
+          copyToRoot = with self.dockerTools; [ caCertificates ];
+        };
+    }; in
     flake-utils.lib.eachDefaultSystem (system: rec {
-      defaultPackage = packages.fvckbot;
-      packages = {
-        fvckbot = nixpkgs.legacyPackages."${system}".haskellPackages.callPackage ./default.nix {};
-        fvckbot-docker = with nixpkgs.legacyPackages."${system}";
-          dockerTools.buildImage {
-            name = "fvckbot";
-            tag = "latest";
-            config.Entrypoint = "${packages.fvckbot}/bin/fvckbot";
-            copyToRoot = with dockerTools; [ caCertificates ];
-          };
-      };
+      packages =
+        let pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; }; in
+        builtins.intersectAttrs (overlay 42 42) pkgs // {
+          default = packages.fvckbot;
+        };
     }) // {
-      overlays.default = self: super: {
-        fvckbot = self.callPackage ./default.nix {};
-      };
+      overlays.default = overlay;
     };
 }
